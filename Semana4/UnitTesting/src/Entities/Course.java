d26ca26aefe5cbca1ded3e665a5fded2a33f16a9
Johanny Espinoza <johanny.espinoza@uamcr.net>
2021-10-06 20:28:36 -0600

    Semana4

---
+++ Semana4/UnitTesting/src/Entities/Course.java
@@ -0,0 +1,27 @@
+package Entities;
+
+public class Course extends Entity{
+    private String name;
+    private Teacher teacher;
+    private double grade;
+    public Course (){}
+    public Course (final int id){
+        super(id);
+    }
+    public Course(final String name){
+        this.name = name;
+    }
+    /*porque acá se hace este constructor 
+    si anteriormente se habian hecho los dos de id y name??*/
+    public Course(final int id, final String name){
+        super(id);
+        this.name = name;
+    }
+    public void setTeacher(final Teacher value){this.teacher= value;}
+    public Teacher getTeacher(){return this.teacher;}
+    public void setName(final String value){this.name = value;}
+    public String getName(){return this.name;}
+    public void setGrade(final double value){this.grade = value;}
+    public double getGrade(){return this.grade;}
+
+}
